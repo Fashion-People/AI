e7ad2a07f25283fbb6a9767516a609a3b4c45797
구르미 <kangbs210210@gmail.com>
2024-03-31 19:08:07 +0900

    Fix : host 에 aws 서버 주소 추가

--- personal_cth/personal_cth/settings.py
+++ personal_cth/personal_cth/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-o&=*hb8cdckss!7)a%i#kep3e3n913_&5r2xk0d-@ty@os(z04
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.34.185.50', 'localhost', '127.0.0.1']
 
 
 # Application definition
